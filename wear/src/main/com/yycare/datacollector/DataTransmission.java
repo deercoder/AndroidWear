c4595d4b78eda13c03c3070a1b492d87be1cc723
Liu Chang <lllccc2009@gmail.com>
2015-11-16 16:59:20 -0500

    simplify the project to fit our project

---
+++ wear/src/main/com/yycare/datacollector/DataTransmission.java
@@ -0,0 +1,20 @@
+package com.yycare.datacollector;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+
+/**
+ * Created by Chang Liu on 11/16/15.
+ */
+public class DataTransmission {
+
+    private SensorManager mSensorManager;
+    public void Init()
+    {
+       // mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+
+    }
+
+
+}
