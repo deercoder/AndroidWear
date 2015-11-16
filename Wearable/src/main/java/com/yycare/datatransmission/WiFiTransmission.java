771c2145b02233dd2b1ecf45e7f12cf8d59c749a
Liu Chang <lllccc2009@gmail.com>
2015-11-16 11:54:30 -0500

    first commit

---
+++ Wearable/src/main/java/com/yycare/datatransmission/WiFiTransmission.java
@@ -0,0 +1,7 @@
+package com.yycare.datatransmission;
+
+/**
+ * Created by deercoderI on 11/16/15.
+ */
+public class WiFiTransmission {
+}
---
+++ Wearable/src/main/java/com/yycare/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.yycare.utils;
+
+/**
+ * Created by Chang Liu on 11/16/15.
+ * For generic manuplating
+ */
+public class Utils {
+
+
+
+
+}
