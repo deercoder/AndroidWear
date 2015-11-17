db72ec8f797ff4d7cf1864ac2d551e2e5933eeca
Liu Chang <lllccc2009@gmail.com>
2015-11-16 23:24:08 -0500

    update a typo

--- wear/src/main/java/yycare/uml/com/mysmartwatchcollector/SensorData/SensorData.java
+++ wear/src/main/java/yycare/uml/com/mysmartwatchcollector/SensorData/SensorData.java
@@ -7,9 +7,6 @@ package yycare.uml.com.mysmartwatchcollector.SensorData;
  */
 public class SensorData {
 
-    // MAX size for the array that stores the sensor data
-    private int MAX_SIZE = 6;
-
     // array for storing one sensor data with multiple dimensions
     private double sensor[];
 
@@ -20,7 +17,7 @@ public class SensorData {
     public SensorData(double val[], int size) {
         maxSize = size;
 
-        sensor = new double[MAX_SIZE];
+        sensor = new double[maxSize];
 
         for (int i = 0; i < size; i++) {
             sensor[i] = val[i];
